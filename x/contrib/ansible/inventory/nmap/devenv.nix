a626e6765d7d019f551e344179640533fd616935
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2024-03-10 17:35:33 -0700

    Cleanup and prepare nmap

---
+++ x/contrib/ansible/inventory/nmap/devenv.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/languages/
+  languages.python = {
+    enable = true;
+    venv.enable = true;
+  };
+
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.ansible
+    pkgs.nmap
+  ];
+
+  enterShell = ''
+    pip install --upgrade pip
+  '';
+
+  # https://devenv.sh/pre-commit-hooks/
+  pre-commit.hooks.shellcheck.enable = false;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
