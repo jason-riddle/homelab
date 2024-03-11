42b491d74fe321eefbc6165af45e6eb97070f91b
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2024-03-10 17:40:57 -0700

    Update k8s

---
+++ x/contrib/ansible/inventory/k8s/devenv.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/languages/
+  languages.python = {
+    enable = true;
+    venv.enable = true;
+  };
+
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.ansible
+  ];
+
+  enterShell = ''
+    pip install --upgrade pip
+  '';
+
+  # https://devenv.sh/pre-commit-hooks/
+  pre-commit.hooks.shellcheck.enable = false;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
