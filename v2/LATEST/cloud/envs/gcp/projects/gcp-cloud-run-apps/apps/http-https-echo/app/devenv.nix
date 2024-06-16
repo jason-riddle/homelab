d9e9a9f4fab92b3fb38ac2d43ff3048f236d0892
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2024-06-16 15:08:14 -0700

    Add http-https-echo

---
+++ v2/LATEST/cloud/envs/gcp/projects/gcp-cloud-run-apps/apps/http-https-echo/app/devenv.nix
@@ -0,0 +1,34 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  # env.ANSIBLE_LOAD_CALLBACK_PLUGINS = "True";
+  # env.ANSIBLE_STDOUT_CALLBACK = "yaml";
+
+  # https://devenv.sh/languages/
+  # languages.python = {
+  #   enable = true;
+  #   venv.enable = true;
+  # };
+
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.google-cloud-sdk
+  ];
+
+  # enterShell = ''
+  #   pip install --upgrade pip
+  #   pip install "esphome==2023.12.9" "pillow==10.1.0"
+  # '';
+
+  # https://devenv.sh/tests/
+  # enterTest = ''
+  #   echo "Running tests"
+  #   git --version | grep "2.42.0"
+  # '';
+
+  # https://devenv.sh/pre-commit-hooks/
+  pre-commit.hooks.shellcheck.enable = false;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
